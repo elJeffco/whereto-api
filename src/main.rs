f54c208c053325058c82ec99d476a8c3e1a95f91
Jeffrey <jeff.boesenkool@gmail.com>
2022-07-18 22:28:20 +0200

    add dotenv support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+use dotenv::dotenv;
 
 // add routes modules and helpers
 mod api;
@@ -11,6 +12,9 @@ use inc::rocket::*;
 
 #[launch]
 async fn rocket() -> _ {
+    // init dotenv variables
+    dotenv().ok();
+
     // init rocket
     rocket::build()
         .register("/", catchers![internal_404, internal_500])
